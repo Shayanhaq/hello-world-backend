4d57cb6e53ac7cb0e311aded84122a915b0e46a0
shayan haq <haqshayan2@gmail.com>
2025-01-06 13:52:37 +0500

    first commit

---
+++ index.mjs
@@ -0,0 +1,23 @@
+import express from 'express'
+import { products } from "./productData.mjs"
+
+import cors from "cors"
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send({name: 'Shayan', age : 21 })
+})
+
+app.get('/Product', (req, res) => {
+    console.log("req", req);
+   
+    // res.send({name: 'Cream', price : 120 , quuantity : "125g" })
+  res.send(products)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
